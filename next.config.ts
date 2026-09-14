f6e966be301b2263ac5499b7c443744e98533a23
asaphtech <asaphsonofberachiah@gmail.com>
2026-09-15 05:56:17 +0700

    Ignore typescript errors on build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Mengabaikan error TypeScript saat proses build di Netlify / Vercel
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
